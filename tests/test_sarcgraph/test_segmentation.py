44f969b90bbb74c16034702f532db12d4c97e80b
saeedmhz <saeedmhz@bu.edu>
2022-09-07 14:57:44 -0400

    fixed test_segmentation.py

--- tests/test_sarcgraph/test_segmentation.py
+++ tests/test_sarcgraph/test_segmentation.py
@@ -1,9 +1,14 @@
-import pytest
-
 from sarcgraph.sarcgraph import SarcGraph
+
 
 def test_detected_zdisc_num():
     sg = SarcGraph(output_dir='test-output', input_type='video')
     filtered_data = sg.preprocessing(input_path='samples/sample_0.avi')
     contours = sg.zdisc_detection(filtered_data)
-    assert len(contours) == 81
+    assert len(contours) == 80
+
+
+def test_segmentation_frames_num():
+    sg = SarcGraph(output_dir='test-output', input_type='video')
+    zdiscs_info = sg.segmentation(input_path='samples/sample_0.avi')
+    assert len(zdiscs_info) == 80
