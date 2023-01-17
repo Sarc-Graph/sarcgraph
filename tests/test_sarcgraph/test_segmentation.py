c5a8e5c52183c6633f58267e9148e26a1cca1dd3
saeedmhz <saeedmhz@bu.edu>
2023-01-17 14:48:08 -0500

    tests-sarcgraph\segmentation is fixed - minor updates on srcgraph.py

--- tests/test_sarcgraph/test_segmentation.py
+++ tests/test_sarcgraph/test_segmentation.py
@@ -1,43 +1,68 @@
-from src.sarcgraph import SarcGraph
+import pytest
 import numpy as np
+import pandas as pd
+import os
+from src.sarcgraph import SarcGraph
+
+sg_vid = SarcGraph("test", "video")
+sg_img = SarcGraph("test", "image")
 
 
-def test_preprocessing():
-    sg = SarcGraph(output_dir="test-output", input_type="video")
-    processed_data_1 = sg.process_input(input_path="samples/sample_0.avi")
-    sg = SarcGraph(output_dir="test-output", input_type="image")
-    processed_data_2 = sg.process_input(input_file=np.zeros((58, 28)))
-    sg = SarcGraph(output_dir="test-output", input_type="video")
-    processed_data_3 = sg.process_input(input_file=np.zeros((4, 58, 28)))
-    assert processed_data_1.shape == (80, 368, 368)
-    assert processed_data_2.shape == (1, 58, 28)
-    assert processed_data_3.shape == (4, 58, 28)
+def test_process_input():
+    filtered_frames_1 = sg_vid._process_input("samples/sample_0.avi")
+    filtered_frames_2 = sg_vid._process_input(raw_frames=np.zeros((4, 58, 28)))
+    filtered_frames_3 = sg_img._process_input(raw_frames=np.zeros((58, 28)))
+    with pytest.raises(TypeError):
+        sg_img._process_input(np.zeros((58, 28)))
+    assert filtered_frames_1.shape == (80, 368, 368)
+    assert filtered_frames_2.shape == (4, 58, 28)
+    assert filtered_frames_3.shape == (1, 58, 28)
+    assert os.path.exists(f"./{sg_vid.output_dir}/raw-frames.npy")
+    assert os.path.exists(f"./{sg_vid.output_dir}/filtered-frames.npy")
 
 
-def test_zdisc_contour_detection():
-    sg = SarcGraph(output_dir="test-output", input_type="video")
-    filtered_data = sg.process_input(input_path="samples/sample_0.avi")
-    contours = sg.detect_contours(filtered_data)
+def test_detect_contours_input_fmt():
+    with pytest.raises(ValueError):
+        SarcGraph()._detect_contours(np.ones((4, 4)))
+    with pytest.raises(ValueError):
+        SarcGraph()._detect_contours(np.ones((1, 4, 4)), min_length="1.0")
+
+
+def test_detect_contours():
+    filtered_frames = sg_vid._process_input("samples/sample_0.avi")
+    contours = sg_vid._detect_contours(filtered_frames)
     assert len(contours) == 80
     assert len(contours[0]) == 81
-    assert contours[0][0].shape[-1] == 2
+    assert os.path.exists(f"./{sg_vid.output_dir}/contours.npy")
 
 
 def test_contour_processor():
-    sg = SarcGraph(output_dir="test-output", input_type="video")
     test_contour_1 = [[-1, -1], [1, 1]]
     test_contour_2 = [[-2, -1], [0, -1], [2, -1], [2, 1], [0, 1], [-2, 1]]
-    processed_contour_1 = sg.process_contour(test_contour_1)
-    processed_contour_2 = sg.process_contour(test_contour_2)
-    assert np.array_equal(processed_contour_1, [0, 0, -1, -1, 1, 1])
-    assert np.array_equal(processed_contour_2, [0, 0, -2, -1, 2, 1])
+    with pytest.raises(ValueError):
+        sg_vid._process_contour(test_contour_1)
+    contour_2 = sg_vid._process_contour(test_contour_2)
+    assert np.array_equal(contour_2, [0, 0, -2, -1, 2, 1])
+
+
+def test_zdiscs_to_pandas():
+    with pytest.raises(ValueError):
+        sg_vid._zdiscs_to_pandas([np.ones((1, 6))])
+    with pytest.raises(ValueError):
+        sg_img._zdiscs_to_pandas([np.ones((1, 6)), np.ones((2, 6))])
+    with pytest.raises(ValueError):
+        sg_img._zdiscs_to_pandas([np.ones((6))])
+    with pytest.raises(TypeError):
+        sg_img._zdiscs_to_pandas([[1, 1, 1, 1, 1, 1]])
+    with pytest.raises(ValueError):
+        sg_img._zdiscs_to_pandas([np.ones((4, 5))])
+    zdiscs_df = sg_img._zdiscs_to_pandas([np.ones((1, 6))])
+    assert isinstance(zdiscs_df, pd.DataFrame)
+    columns = ["frame", "x", "y", "p1_x", "p1_y", "p2_x", "p2_y"]
+    assert set(zdiscs_df.columns) == set(columns)
 
 
 def test_zdisc_segmentation():
-    sg = SarcGraph(output_dir="test-output", input_type="video")
-    zdiscs = sg.zdisc_segmentation(input_path="samples/sample_0.avi")
-    assert np.array_equal(
-        zdiscs.columns, ["frame", "x", "y", "p1_x", "p1_y", "p2_x", "p2_y"]
-    )
-    assert len(zdiscs.frame.unique()) == 80
-    assert len(zdiscs.loc[zdiscs.frame == 0]) == 81
+    zdiscs = sg_vid.zdisc_segmentation("samples/sample_0.avi")
+    assert zdiscs.frame.max() == 79
+    assert len(zdiscs[zdiscs.frame == 0]) == 81
