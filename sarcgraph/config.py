37a2dd917fcdccde1bfbd8fe8abc4d89894a38b8
saeedmhz <saeedmhz@bu.edu>
2023-11-02 16:44:58 -0400

    Refactor config management to use getter/setter methods
    This commit overhauls the configuration handling by introducing getter and setter methods for all configuration options. This change enables better type and value validation, ensuring that the configuration options are set with appropriate values throughout the application lifecycle.
    
    Key changes include:
    - Each configuration option is now accessed via a getter method, ensuring encapsulation and the ability to perform checks when retrieving the value.
    - Setter methods are introduced for all configuration options. These methods include type checks and validations to prevent the assignment of invalid or inappropriate values.
    - A new `print` function is added to the `Config` class to allow users to print out all current configuration options and their values. This function ensures that only the public attribute names are displayed (without the private underscore prefix), providing a clean and user-friendly output.
    
    The use of getter/setter methods enhances the robustness of the configuration system by enforcing data integrity and making the codebase more maintainable and easy to debug.

--- sarcgraph/config.py
+++ sarcgraph/config.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Optional
 
 
@@ -56,80 +56,258 @@ class Config:
     Defaults to 1.2.
     :type angle_threshold: float
     """
-
-    output_dir: str = "output"
-    input_type: str = "video"
-    input_file: Optional[str] = None
-    save_output: bool = True
-    gaussian_sigma: float = 1.0
-    zdisc_min_length: int = 15
-    zdisc_max_length: int = 50
-    full_track_ratio: float = 0.75
-    tp_depth: int = 4
-    skip_merge: bool = False
-    num_neighbors: int = 3
-    avg_sarc_length: float = 15.0
-    max_sarc_length: float = 30.0
-    coeff_avg_length: float = 1.0
-    coeff_neighbor_length: float = 1.0
-    coeff_neighbor_angle: float = 1.0
-    score_threshold: float = 0.1
-    angle_threshold: float = 1.2
+    _output_dir: str = field(default="output", init=False)
+    _input_type: str = field(default="video", init=False)
+    _input_file: Optional[str] = field(default=None, init=False)
+    _save_output: bool = field(default=True, init=False)
+    _gaussian_sigma: float = field(default=1.0, init=False)
+    _zdisc_min_length: int = field(default=15, init=False)
+    _zdisc_max_length: int = field(default=50, init=False)
+    _full_track_ratio: float = field(default=0.75, init=False)
+    _tp_depth: int = field(default=4, init=False)
+    _skip_merge: bool = field(default=False, init=False)
+    _num_neighbors: int = field(default=3, init=False)
+    _avg_sarc_length: float = field(default=15.0, init=False)
+    _max_sarc_length: float = field(default=30.0, init=False)
+    _coeff_avg_length: float = field(default=1.0, init=False)
+    _coeff_neighbor_length: float = field(default=1.0, init=False)
+    _coeff_neighbor_angle: float = field(default=1.0, init=False)
+    _score_threshold: float = field(default=0.1, init=False)
+    _angle_threshold: float = field(default=1.2, init=False)
 
     VALID_INPUT_TYPES = ["image", "video"]
 
     def __post_init__(self):
-        # Type checks
-        if not isinstance(self.output_dir, str):
-            raise TypeError("output_dir must be a string")
-        if not isinstance(self.input_type, str):
-            raise TypeError("input_type must be a string")
-        if not (self.input_file is None or isinstance(self.input_file, str)):
-            raise TypeError("input_file must be a string or None")
-        if not isinstance(self.save_output, bool):
-            raise TypeError("save_output must be a boolean")
-        if not isinstance(self.gaussian_sigma, float):
-            raise TypeError("gaussian_sigma must be a float")
-        if not isinstance(self.zdisc_min_length, int):
-            raise TypeError("zdisc_min_length must be an integer")
-        if not isinstance(self.zdisc_max_length, int):
-            raise TypeError("zdisc_max_length must be an integer")
-        if not isinstance(self.full_track_ratio, float):
-            raise TypeError("full_track_ratio must be a float")
-        if not isinstance(self.tp_depth, int):
-            raise TypeError("tp_depth must be an integer")
-        if not isinstance(self.skip_merge, bool):
-            raise TypeError("skip_merge must be a boolean")
-        if not isinstance(self.num_neighbors, int):
-            raise TypeError("num_neighbors must be an integer")
-        if not isinstance(self.avg_sarc_length, float):
-            raise TypeError("avg_sarc_length must be a float")
-        if not isinstance(self.max_sarc_length, float):
-            raise TypeError("max_sarc_length must be a float")
-        if not isinstance(self.coeff_avg_length, float):
-            raise TypeError("coeff_avg_length must be a float")
-        if not isinstance(self.coeff_neighbor_length, float):
-            raise TypeError("coeff_neighbor_length must be a float")
-        if not isinstance(self.coeff_neighbor_angle, float):
-            raise TypeError("coeff_neighbor_angle must be a float")
-        if not isinstance(self.score_threshold, float):
-            raise TypeError("score_threshold must be a float")
-        if not isinstance(self.angle_threshold, float):
-            raise TypeError("angle_threshold must be a float")
+        self.output_dir = self._output_dir
+        self.input_type = self._input_type
+        self.input_file = self._input_file
+        self.save_output = self._save_output
+        self.gaussian_sigma = self._gaussian_sigma
+        self.zdisc_min_length = self._zdisc_min_length
+        self.zdisc_max_length = self._zdisc_max_length
+        self.full_track_ratio = self._full_track_ratio
+        self.tp_depth = self._tp_depth
+        self.skip_merge = self._skip_merge
+        self.num_neighbors = self._num_neighbors
+        self.avg_sarc_length = self._avg_sarc_length
+        self.max_sarc_length = self._max_sarc_length
+        self.coeff_avg_length = self._coeff_avg_length
+        self.coeff_neighbor_length = self._coeff_neighbor_length
+        self.coeff_neighbor_angle = self._coeff_neighbor_angle
+        self.score_threshold = self._score_threshold
+        self.angle_threshold = self._angle_threshold
 
-        # Value checks
-        if self.input_type not in self.VALID_INPUT_TYPES:
-            raise ValueError("input_type must be one of "
-                             f"{self.VALID_INPUT_TYPES}")
-        if self.zdisc_max_length <= self.zdisc_min_length:
+    @property
+    def output_dir(self) -> str:
+        return self._output_dir
+
+    @output_dir.setter
+    def output_dir(self, value: str):
+        if not isinstance(value, str):
+            raise TypeError("output_dir must be a string")
+        self._output_dir = value
+
+    @property
+    def input_type(self) -> str:
+        return self._input_type
+
+    @input_type.setter
+    def input_type(self, value: str):
+        if not isinstance(value, str):
+            raise TypeError("input_type must be a string")
+        self._input_type = value
+
+    @property
+    def input_file(self) -> Optional[str]:
+        return self._input_file
+
+    @input_file.setter
+    def input_file(self, value: Optional[str]):
+        if value is not None and not isinstance(value, str):
+            raise TypeError("input_file must be a string")
+        self._input_file = value
+
+    @property
+    def save_output(self) -> bool:
+        return self._save_output
+
+    @save_output.setter
+    def save_output(self, value: bool):
+        if not isinstance(value, bool):
+            raise TypeError("save_output must be a boolean")
+        self._save_output = value
+
+    @property
+    def gaussian_sigma(self) -> float:
+        return self._gaussian_sigma
+
+    @gaussian_sigma.setter
+    def gaussian_sigma(self, value: float):
+        if not isinstance(value, float):
+            raise TypeError("gaussian_sigma must be a float")
+        if value <= 0.0:
+            raise ValueError("gaussian_sigma must be greater than 0.0")
+        self._gaussian_sigma = value
+
+    @property
+    def zdisc_min_length(self) -> int:
+        return self._zdisc_min_length
+
+    @zdisc_min_length.setter
+    def zdisc_min_length(self, value: int):
+        if not isinstance(value, int):
+            raise TypeError("zdisc_min_length must be an integer")
+        if value <= 2:
+            raise ValueError("zdisc_min_length must be greater than 2")
+        self._zdisc_min_length = value
+
+    @property
+    def zdisc_max_length(self) -> int:
+        return self._zdisc_max_length
+
+    @zdisc_max_length.setter
+    def zdisc_max_length(self, value: int):
+        if not isinstance(value, int):
+            raise TypeError("zdisc_max_length must be an integer")
+        if value <= self.zdisc_min_length:
             raise ValueError("zdisc_max_length must be greater than "
                              "zdisc_min_length")
-        if not 0.5 <= self.full_track_ratio <= 1.0:
+        self._zdisc_max_length = value
+
+    @property
+    def full_track_ratio(self) -> float:
+        return self._full_track_ratio
+
+    @full_track_ratio.setter
+    def full_track_ratio(self, value: float):
+        if not isinstance(value, float):
+            raise TypeError("full_track_ratio must be a float")
+        if not 0.5 <= value <= 1.0:
             raise ValueError("full_track_ratio must be between 0.5 and 1.0")
-        if self.max_sarc_length <= self.avg_sarc_length:
+        self._full_track_ratio = value
+
+    @property
+    def tp_depth(self) -> int:
+        return self._tp_depth
+
+    @tp_depth.setter
+    def tp_depth(self, value: int):
+        if not isinstance(value, int):
+            raise TypeError("tp_depth must be an integer")
+        if value <= 0:
+            raise ValueError("tp_depth must be greater than 0")
+        self._tp_depth = value
+
+    @property
+    def skip_merge(self) -> bool:
+        return self._skip_merge
+
+    @skip_merge.setter
+    def skip_merge(self, value: bool):
+        if not isinstance(value, bool):
+            raise TypeError("skip_merge must be a boolean")
+        self._skip_merge = value
+
+    @property
+    def num_neighbors(self) -> int:
+        return self._num_neighbors
+
+    @num_neighbors.setter
+    def num_neighbors(self, value: int):
+        if not isinstance(value, int):
+            raise TypeError("num_neighbors must be an integer")
+        if value <= 1:
+            raise ValueError("num_neighbors must be greater than 1")
+        self._num_neighbors = value
+
+    @property
+    def avg_sarc_length(self) -> float:
+        return self._avg_sarc_length
+
+    @avg_sarc_length.setter
+    def avg_sarc_length(self, value: float):
+        if not isinstance(value, float):
+            raise TypeError("avg_sarc_length must be a float")
+        if value <= 0.0:
+            raise ValueError("avg_sarc_length must be greater than 0.0")
+        self._avg_sarc_length = value
+
+    @property
+    def max_sarc_length(self) -> float:
+        return self._max_sarc_length
+
+    @max_sarc_length.setter
+    def max_sarc_length(self, value: float):
+        if not isinstance(value, float):
+            raise TypeError("max_sarc_length must be a float")
+        if value <= self.avg_sarc_length:
             raise ValueError("max_sarc_length must be greater than "
                              "avg_sarc_length")
-        if self.score_threshold <= 0.0:
+        self._max_sarc_length = value
+
+    @property
+    def coeff_avg_length(self) -> float:
+        return self._coeff_avg_length
+
+    @coeff_avg_length.setter
+    def coeff_avg_length(self, value: float):
+        if not isinstance(value, float):
+            raise TypeError("coeff_avg_length must be a float")
+        if value <= 0.0:
+            raise ValueError("coeff_avg_length must be greater than 0.0")
+        self._coeff_avg_length = value
+
+    @property
+    def coeff_neighbor_length(self) -> float:
+        return self._coeff_neighbor_length
+
+    @coeff_neighbor_length.setter
+    def coeff_neighbor_length(self, value: float):
+        if not isinstance(value, float):
+            raise TypeError("coeff_neighbor_length must be a float")
+        if value <= 0.0:
+            raise ValueError("coeff_neighbor_length must be greater than 0.0")
+        self._coeff_neighbor_length = value
+
+    @property
+    def coeff_neighbor_angle(self) -> float:
+        return self._coeff_neighbor_angle
+
+    @coeff_neighbor_angle.setter
+    def coeff_neighbor_angle(self, value: float):
+        if not isinstance(value, float):
+            raise TypeError("coeff_neighbor_angle must be a float")
+        if value <= 0.0:
+            raise ValueError("coeff_neighbor_angle must be greater than 0.0")
+        self._coeff_neighbor_angle = value
+
+    @property
+    def score_threshold(self) -> float:
+        return self._score_threshold
+
+    @score_threshold.setter
+    def score_threshold(self, value: float):
+        if not isinstance(value, float):
+            raise TypeError("score_threshold must be a float")
+        if value <= 0.0:
             raise ValueError("score_threshold must be greater than 0.0")
-        if self.angle_threshold <= 1.0:
+        self._score_threshold = value
+
+    @property
+    def angle_threshold(self) -> float:
+        return self._angle_threshold
+
+    @angle_threshold.setter
+    def angle_threshold(self, value: float):
+        if not isinstance(value, float):
+            raise TypeError("angle_threshold must be a float")
+        if value <= 1.0:
             raise ValueError("angle_threshold must be greater than 1.0")
+        self._angle_threshold = value
+
+    def print(self):
+        for attribute in self.__dict__:
+            # Strip the leading underscore and get the property's value using its getter.
+            public_attribute_name = attribute.lstrip('_')
+            value = getattr(self, public_attribute_name)
+            print(f"{public_attribute_name} = {value}")
